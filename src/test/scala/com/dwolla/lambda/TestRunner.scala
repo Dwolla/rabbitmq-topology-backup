e376633e860582955016a970e85f3ed6b6c88d17
Brian P. Holt <bholt@dwolla.com>
2019-08-07 17:58:47 -0500

    accept rabbitmq hostname and kms-encrypted credentials via lambda input

--- src/test/scala/com/dwolla/lambda/TestRunner.scala
+++ src/test/scala/com/dwolla/lambda/TestRunner.scala
@@ -2,16 +2,21 @@ package com.dwolla.lambda
 
 import java.io._
 
-import com.dwolla.rabbitmq.topology.Lambda
+import com.dwolla.rabbitmq.topology.LambdaHandler
 import io.circe.Printer
 import io.circe.literal._
 
 object TestRunner extends App {
 
-  val input: InputStream = new ByteArrayInputStream(json"null".noSpaces.getBytes)
+  val input: InputStream = new ByteArrayInputStream(
+    json"""{
+             "hostname": "https://rabbit.us-west-2.devint.dwolla.net",
+             "username": "guest",
+             "password": "AQICAHh38+DAqADvcRLU4+t2AYhr82YbZuuFQdjdX95NTppHhwHd8XtgUIF6t8gP+mKlCrizAAAAYzBhBgkqhkiG9w0BBwagVDBSAgEAME0GCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMuvCOEA4D/QGIaihbAgEQgCATYPnSoUh0UI+QsqlR00kP7cGLdyh6fUrfBv7Gzt8ToA=="
+           }""".noSpaces.getBytes)
   val output: ByteArrayOutputStream = new ByteArrayOutputStream()
 
-  new Lambda(Printer.spaces2).handleRequest(input, output, null)
+  new LambdaHandler(Printer.spaces2).handleRequest(input, output, null)
 
   println(output)
 }
